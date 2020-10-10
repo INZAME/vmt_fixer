ae7eefb99134e24217d891471d13d8a9d4bcde12
Alexey <inzame1k14@gmail.com>
2020-10-10 15:54:35 +0300

    first version

---
+++ VMT_Fixer/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace VMT_Fixer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.selectFolderButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.fixButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // selectFolderButton
+            // 
+            this.selectFolderButton.Location = new System.Drawing.Point(230, 77);
+            this.selectFolderButton.Name = "selectFolderButton";
+            this.selectFolderButton.Size = new System.Drawing.Size(116, 36);
+            this.selectFolderButton.TabIndex = 0;
+            this.selectFolderButton.Text = "Select folder";
+            this.selectFolderButton.UseVisualStyleBackColor = true;
+            this.selectFolderButton.Click += new System.EventHandler(this.selectFolderButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(73, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(151, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // fixButton
+            // 
+            this.fixButton.Location = new System.Drawing.Point(145, 119);
+            this.fixButton.Name = "fixButton";
+            this.fixButton.Size = new System.Drawing.Size(116, 36);
+            this.fixButton.TabIndex = 0;
+            this.fixButton.Text = "Fix VMTs";
+            this.fixButton.UseVisualStyleBackColor = true;
+            this.fixButton.Click += new System.EventHandler(this.fixButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Simple tool for fixing VMTs files for SFM usage";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(340, 171);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(62, 13);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "by INZAME";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 193);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.fixButton);
+            this.Controls.Add(this.selectFolderButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "VMT Fixer by INZAME";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button selectFolderButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button fixButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ VMT_Fixer/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VMT_Fixer
+{
+    public partial class Form1 : Form
+    {
+        private string folderName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool fixFile(string filePath)
+        {
+            List<string> _keyWords = new List<string>()
+            {
+                "\"character\"",
+                "\"vertexlitgeneric\""
+            };
+            var _lines = File.ReadAllLines(filePath);
+            if (_keyWords.Contains(_lines[0])) {
+                _lines[0] = "\"VertexLitGeneric\"";
+            }
+            else
+            {
+                return false;
+            }
+            try
+            {
+                File.WriteAllLines(filePath, _lines);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($"Error writing {filePath}.", @"VMT Error");
+                return false;
+            }
+            return true;
+        } 
+
+        private string[] getFiles(string path)
+        {
+            string[] _filesPaths = Directory.GetFiles(path, "*.vmt", SearchOption.AllDirectories);
+            return _filesPaths;
+        }
+
+        private void selectFolderButton_Click(object sender, EventArgs e)
+        {
+            var result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                folderName = folderBrowserDialog1.SelectedPath;
+                textBox1.Text = folderName;
+            }
+        }
+
+        private void fixButton_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(folderName))
+            {
+                MessageBox.Show(@"Please select working directory.", @"VMT Dialog");
+                return;
+            }
+            var result = MessageBox.Show(@"Now gonna start fixing VMT files", @"VMT Dialog");
+            if (result == DialogResult.OK)
+            {
+                var files = getFiles(folderName);
+                foreach (var file in files)
+                {
+                    fixFile(file);
+                    // Do smthing 
+                }
+                MessageBox.Show(@"Done!", @"VMT Dialog");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "Select folder";
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://www.youtube.com/user/jzteVo");
+        }
+    }
+}
